f41586ab50ed8c3a3d95edb53c07e0b1dde8b294
unknown <vuthanhdat019@gmail.com>
2024-05-22 11:17:31 +0700

    subfinal1

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
 import pygame
 
-from game.Board import Board
-
-pygame.init()
-
-WINDOW_SIZE = (600, 600)
-screen = pygame.display.set_mode(WINDOW_SIZE)
-
-board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1])
-
-
-def draw(display):
-    display.fill((255, 255, 255))
-    board.draw(display)
-    pygame.display.update()
-
+from components.Game import Game
 
 if __name__ == "__main__":
-    running = True
-    while running:
-        mx, my = pygame.mouse.get_pos()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    board.handle_click(mx, my)
-
-        if board.is_in_checkmate("black"):
-            print("White wins")
-            running = False
-        elif board.is_in_checkmate("white"):
-            print("Black wins")
-            running = False
-
-        draw(screen)
+    width = 960
+    height = 640
+    square_size = 80
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    game = Game(width, height, square_size, screen)
+    game.display()
+    pygame.quit()
